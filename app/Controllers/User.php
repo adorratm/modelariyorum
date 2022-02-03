b89d2be58e4c88999ac8dfe0f78df5b140680224
Emre KILIÇ <emrekilic19983@gmail.com>
2022-02-03 12:45:08 +0300

    updated user oauth login and register

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use \App\Libraries\Oauth;
 use \OAuth2\Request;
 use CodeIgniter\API\ResponseTrait;
+use App\Models\UserModel;
 
 class User extends BaseController
 {
@@ -18,5 +19,38 @@ class User extends BaseController
         $code = $respond->getStatusCode();
         $body = $respond->getResponseBody();
         return $this->respond(json_decode($body), $code);
+    }
+
+    public function register()
+    {
+        helper("form");
+        if ($this->request->getMethod() != 'post') :
+            return $this->fail('Only post request is allowed.');
+        endif;
+
+        $rules = [
+            'firstname' => 'required|min_length[2]|max_length[70]',
+            'lastname' => 'required|min_length[2]|max_length[70]',
+            'email' => 'required|valid_email|is_unique[users.email]|max_length[255]|min_length[2]',
+            'password' => 'required|min_length[6]|max_length[255]',
+            'password_confirm' => 'matches[password]',
+        ];
+
+        if(!$this->validate($rules)):
+            return $this->failValidationErrors($this->validator->getErrors());
+        endif;
+
+        $model = new UserModel();
+        $data = [
+            'firstname' => $this->request->getVar('firstname'),
+            'lastname' => $this->request->getVar('lastname'),
+            'email' => $this->request->getVar('email'),
+            'password' => $this->request->getVar('password'),
+        ];
+
+        $user_id = $model->insert($data);
+        $data['id'] = $user_id;
+        unset($data["password"]);
+        return $this->respondCreated($data); 
     }
 }
--- app/Libraries/Oauth.php
+++ app/Libraries/Oauth.php
@@ -2,8 +2,8 @@
 
 namespace App\Libraries;
 
-use \OAuth2\Storage\Pdo;
-
+//use \OAuth2\Storage\Pdo;
+use App\Libraries\CustomOauthStorage;
 class Oauth
 {
     var $server;
@@ -19,7 +19,7 @@ class Oauth
         $username = getenv("database.default.username");
         $password = getenv("database.default.password");
 
-        $storage = new Pdo(["dsn" => $dsn, "username" => $username, "password" => $password]);
+        $storage = new CustomOauthStorage(["dsn" => $dsn, "username" => $username, "password" => $password]);
         $this->server = new \OAuth2\Server($storage);
         $this->server->addGrantType(new \OAuth2\GrantType\UserCredentials($storage));
     }
