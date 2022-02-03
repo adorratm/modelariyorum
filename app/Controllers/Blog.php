76651f7f0ab78c443095b852fa9b7ee3143d247b
Emre KILIÇ <emrekilic19983@gmail.com>
2022-02-03 10:06:27 +0300

    Authorization not implemented yet.

--- app/Controllers/Blog.php
+++ app/Controllers/Blog.php
@@ -9,20 +9,27 @@ class Blog extends ResourceController
     protected $modelName = 'App\Models\BlogModel';
     protected $format = 'json';
 
+    /**
+     * Get All Blogs
+     */
     public function index()
     {
         $posts = $this->model->findAll();
         return $this->respond($posts);
     }
 
-    public function create(){
+    /**
+     * Create Blog
+     */
+    public function create()
+    {
         helper(['form']);
         $rules = [
             'title' => 'required|min_length[2]',
             'description' => 'required'
         ];
 
-        if(!$this->validate($rules)):
+        if (!$this->validate($rules)) :
             return $this->failValidationErrors($this->validator->getErrors());
         endif;
 
@@ -35,16 +42,50 @@ class Blog extends ResourceController
         return $this->respondCreated($data);
     }
 
-    public function update($id = null){
+    /**
+     * Get Blog
+     */
+    public function show($id = null)
+    {
+        $data = $this->model->find($id);
+        return $this->respond($data);
+    }
+
+    /**
+     * Update Blog
+     */
+    public function update($id = null)
+    {
         helper(['form']);
         $rules = [
             'title' => 'required|min_length[2]',
             'description' => 'required'
         ];
-        if(!$this->validate($rules)):
+        if (!$this->validate($rules)) :
             return $this->failValidationErrors($this->validator->getErrors());
         endif;
 
-        
+        $input = $this->request->getRawInput();
+        $data = [
+            'post_id' => $id,
+            'post_title' => $input["title"],
+            'post_description' => $input["description"],
+        ];
+
+        $this->model->save($data);
+        return $this->respond($data);
+    }
+
+    /**
+     * Delete Blog
+     */
+    public function delete($id = null)
+    {
+        $data = $this->model->find($id);
+        if(!empty($data)):
+            $this->model->delete($id);
+            return $this->respondDeleted($data);
+        endif;
+        return $this->failNotFound('Item Not Found.');
     }
 }
