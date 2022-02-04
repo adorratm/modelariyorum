682661147f35864358b0f4eb1698a59f2adbce48
Emre KILIÇ <emrekilic19983@gmail.com>
2022-02-04 15:55:48 +0300

    getmethod to getPost

--- app/Controllers/Backend/User.php
+++ app/Controllers/Backend/User.php
@@ -37,41 +37,41 @@ class User extends BaseController
          * Load Helpers
          */
         helper("form");
-        if ($this->request->getMethod() != 'post') :
-            return $this->fail('Only post request is allowed.');
+        if ($this->request->getPost()) :
+            /**
+             * Validation Rules
+             */
+            $rules = [
+                'firstname' => 'required|min_length[2]|max_length[70]',
+                'lastname' => 'required|min_length[2]|max_length[70]',
+                'email' => 'required|valid_email|is_unique[users.email]|max_length[255]|min_length[2]',
+                'password' => 'required|min_length[6]|max_length[255]',
+                'password_confirm' => 'matches[password]',
+            ];
+            /**
+             * Validation
+             */
+            if (!$this->validate($rules)) :
+                return $this->failValidationErrors($this->validator->getErrors());
+            endif;
+            /**
+             * Prepare Data
+             */
+            $model = new UserModel();
+            $data = [
+                'firstname' => $this->request->getVar('firstname'),
+                'lastname' => $this->request->getVar('lastname'),
+                'email' => $this->request->getVar('email'),
+                'password' => $this->request->getVar('password'),
+            ];
+            $user_id = $model->insert($data);
+            $data['id'] = $user_id;
+            unset($data["password"]);
+            /**
+             * Return Response
+             */
+            return $this->respondCreated($data);
         endif;
-        /**
-         * Validation Rules
-         */
-        $rules = [
-            'firstname' => 'required|min_length[2]|max_length[70]',
-            'lastname' => 'required|min_length[2]|max_length[70]',
-            'email' => 'required|valid_email|is_unique[users.email]|max_length[255]|min_length[2]',
-            'password' => 'required|min_length[6]|max_length[255]',
-            'password_confirm' => 'matches[password]',
-        ];
-        /**
-         * Validation
-         */
-        if (!$this->validate($rules)) :
-            return $this->failValidationErrors($this->validator->getErrors());
-        endif;
-        /**
-         * Prepare Data
-         */
-        $model = new UserModel();
-        $data = [
-            'firstname' => $this->request->getVar('firstname'),
-            'lastname' => $this->request->getVar('lastname'),
-            'email' => $this->request->getVar('email'),
-            'password' => $this->request->getVar('password'),
-        ];
-        $user_id = $model->insert($data);
-        $data['id'] = $user_id;
-        unset($data["password"]);
-        /**
-         * Return Response
-         */
-        return $this->respondCreated($data);
+        return $this->fail('Only post request is allowed.');
     }
 }
