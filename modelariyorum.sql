40614e85b10915b8027565fce839795fd36ce039
Emre KILIÇ <emrekilic19983@gmail.com>
2022-03-19 11:05:44 +0300

    sql update

--- modelariyorum.sql
+++ modelariyorum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 27, 2022 at 07:18 PM
+-- Generation Time: Mar 19, 2022 at 09:03 AM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.2
 
